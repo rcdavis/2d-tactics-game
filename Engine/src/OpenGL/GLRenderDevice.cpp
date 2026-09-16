3112c08ae647b824d3d41090e58088e88115c5ba
Ren Davis <rcdavisgd@gmail.com>
2026-09-16 11:01:48 -0400

    Add loading of OpenGL functions

--- Engine/src/OpenGL/GLRenderDevice.cpp
+++ Engine/src/OpenGL/GLRenderDevice.cpp
@@ -1,8 +1,11 @@
 #include "OpenGL/GLRenderDevice.h"
 
 #include "IWindow.h"
+#include "SDL3/SDL_video.h"
 #include "SDL3/SDL_error.h"
 #include "Utils/Log.h"
+
+#include "glad/glad.h"
 
 GLRenderDevice::~GLRenderDevice() {
 	Shutdown();
@@ -13,6 +16,11 @@ bool GLRenderDevice::Init(IWindow* window) {
 	mContext = SDL_GL_CreateContext(sdlWindow);
 	if (!mContext) {
 		LOG_ERROR("Failed to create OpenGL context: {}", SDL_GetError());
+		return false;
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+		LOG_ERROR("Failed to initialize OpenGL loader");
 		return false;
 	}
 
