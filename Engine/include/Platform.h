3e208cd50f58a6d9e1283be0e200340a4be92d1f
Ren Davis <rcdavisgd@gmail.com>
2026-09-16 11:47:38 -0400

    Create Render device and OpenGL loading (#6)
    * Create Render device
    
    * Add loading of OpenGL functions
    
    * Add OpenGL present
    
    * Add support for vsync
    
    * Update usage of vsync

--- Engine/include/Platform.h
+++ Engine/include/Platform.h
@@ -1,10 +1,12 @@
 #pragma once
 
+class IRenderDevice;
 class IWindow;
 struct PlatformEvent;
 struct WindowCreateInfo;
 
 struct Platform {
+	IRenderDevice* renderDevice = nullptr;
 	IWindow* window = nullptr;
 
 	bool Init(const WindowCreateInfo& info);
---
+++ Engine/src/OpenGL/GLRenderDevice.cpp
@@ -0,0 +1,46 @@
+#include "OpenGL/GLRenderDevice.h"
+
+#include "IWindow.h"
+#include "SDL3/SDL_video.h"
+#include "SDL3/SDL_error.h"
+#include "Utils/Log.h"
+
+#include "glad/glad.h"
+
+GLRenderDevice::~GLRenderDevice() {
+	Shutdown();
+}
+
+bool GLRenderDevice::Init(IWindow* window) {
+	mWindow = static_cast<SDL_Window*>(window->GetHandle());
+	mContext = SDL_GL_CreateContext(mWindow);
+	if (!mContext) {
+		LOG_ERROR("Failed to create OpenGL context: {}", SDL_GetError());
+		return false;
+	}
+
+	if (!SDL_GL_MakeCurrent(mWindow, mContext)) {
+		LOG_ERROR("Failed to make OpenGL context current: {}", SDL_GetError());
+		return false;
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+		LOG_ERROR("Failed to initialize OpenGL loader");
+		return false;
+	}
+
+	return true;
+}
+
+void GLRenderDevice::Shutdown() {
+	if (mContext) {
+		SDL_GL_DestroyContext(mContext);
+		mContext = nullptr;
+	}
+
+	mWindow = nullptr;
+}
+
+void GLRenderDevice::Present() {
+	SDL_GL_SwapWindow(mWindow);
+}
