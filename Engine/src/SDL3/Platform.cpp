08f5fdedb4995891d4b84c24a27229995a11a194
Ren Davis <rcdavisgd@gmail.com>
2026-09-16 06:39:02 -0400

    Added Platform events (#4)

--- Engine/src/SDL3/Platform.cpp
+++ Engine/src/SDL3/Platform.cpp
@@ -2,7 +2,10 @@
 
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_error.h"
+#include "SDL3/SDL_events.h"
 #include "Utils/Log.h"
+
+#include "PlatformEvent.h"
 
 bool Platform::Init(const WindowDesc& desc) {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -22,4 +25,22 @@ void Platform::Destroy() {
 	window.Destroy();
 
 	SDL_Quit();
+}
+
+bool Platform::PollEvent(PlatformEvent& event) {
+	SDL_Event sdlEvent {};
+	if (SDL_PollEvent(&sdlEvent)) {
+		switch (sdlEvent.type) {
+		case SDL_EVENT_QUIT:
+			event.type = PlatformEvent::Type::Quit;
+			break;
+		default:
+			event.type = PlatformEvent::Type::Count;
+			break;
+		}
+
+		return true;
+	}
+
+	return false;
 }
