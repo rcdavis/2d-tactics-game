618ea618955786fc3d085518370788dbdad57f61
Ren Davis <rcdavisgd@gmail.com>
2026-09-19 11:44:57 -0400

    Added pathing for textures

--- AssetBuilder/src/AssetBuilder.cpp
+++ AssetBuilder/src/AssetBuilder.cpp
@@ -45,7 +45,28 @@ void AssetBuilder::CreateTextureIdHeader(const std::filesystem::path& inputDir, 
 		file << "\t\t" << texture.stem().string() << ",\n";
 	}
 	file << "\t\tCount\n";
-	file << "\t};\n";
+	file << "\t};\n\n";
+
+	file << "\tinline constexpr const char* ToString(Id id) {\n";
+	file << "\t\tswitch (id) {\n";
+	for (const auto& path : mTextures) {
+		file << "\t\t\tcase Id::" << path.stem().string() << ": return \"" << path.stem().string() << "\";\n";
+	}
+	file << "\t\t\tdefault: return \"Unknown\";\n";
+	file << "\t\t}\n";
+	file << "\t}\n";
+
+	const auto resParentDir = inputDir / "..";
+	file << "\n\tinline constexpr const char* GetPath(Id id) {\n";
+	file << "\t\tswitch (id) {\n";
+	for (const auto& path : mTextures) {
+		const auto relativePath = std::filesystem::relative(path, resParentDir);
+		file << "\t\t\tcase Id::" << path.stem().string() << ": return \"" << relativePath.generic_string() << "\";\n";
+	}
+	file << "\t\t\tdefault: return nullptr;\n";
+	file << "\t\t}\n";
+	file << "\t}\n";
+
 	file << "} // namespace Res::Textures\n";
 
 	std::cout << "Generated texture ID header at " << headerPath << std::endl;
