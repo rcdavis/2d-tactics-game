8c2eda0770a54bf7fc327e00f4e7ca25cc646848
Ren Davis <rcdavisgd@gmail.com>
2026-09-19 10:16:34 -0400

    Added starting TextureSystem

---
+++ Engine/include/TextureSystem.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace TextureSystem {
+	bool Init();
+
+	void Shutdown();
+}
---
+++ Engine/src/TextureSystem.cpp
@@ -0,0 +1,12 @@
+#include "TextureSystem.h"
+
+namespace TextureSystem {
+	bool Init() {
+		// Initialization code here
+		return true;
+	}
+
+	void Shutdown() {
+		// Shutdown code here
+	}
+}
