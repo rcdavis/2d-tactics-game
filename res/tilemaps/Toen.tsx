abdc1d4bfd506756cf32113d41d5a6f8834d5906
Ren Davis <rcdavisgd@gmail.com>
2026-09-22 07:03:50 -0400

    Added Tiled map

---
+++ res/tilemaps/Toen.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Toen" tilewidth="16" tileheight="16" tilecount="364" columns="7">
+ <image source="../repos/2d-tactics-game/res/textures/ToenTileSet.png" width="112" height="832"/>
+ <tile id="0">
+  <properties>
+   <property name="movementCost" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="movementCost" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="movementCost" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="movementCost" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="movementCost" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="movementCost" type="int" value="50"/>
+  </properties>
+ </tile>
+</tileset>
