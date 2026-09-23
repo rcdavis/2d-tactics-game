8c19fa5a9de484da8a648d2bfd74694ea8ac766e
Ren Davis <rcdavisgd@gmail.com>
2026-09-23 01:48:36 -0400

    Added tile map data objects

---
+++ Engine/include/Tiles/TileMap.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "TextureHandle.h"
+
+struct TileTerrain {
+	uint8_t movementCost = 0;
+};
+
+struct TileSet {
+	TileTerrain* terrains = nullptr;
+	uint16_t terrainCount = 0;
+	uint16_t tileWidth = 0;
+	uint16_t tileHeight = 0;
+	TextureHandle textureHandle = InvalidTextureHandle;
+	uint8_t columnCount = 0;
+
+	bool Init(const char* const filepath);
+	void Destroy();
+};
+
+struct TileLayer {
+	uint16_t* tiles = nullptr;
+};
+
+struct TileMap {
+	TileLayer* layers = nullptr;
+	uint8_t layerCount = 0;
+	uint8_t tileSetHandle = -1;
+
+	uint16_t tileRowCount = 0;
+	uint16_t tileColumnCount = 0;
+
+	uint16_t tileWidth = 0;
+	uint16_t tileHeight = 0;
+
+	bool Init(const char* const filepath);
+	void Destroy();
+};
---
+++ Engine/src/Tiles/TileMap.cpp
@@ -0,0 +1,19 @@
+#include "Tiles/TileMap.h"
+
+bool TileSet::Init(const char* const filepath) {
+	// Implementation goes here
+	return true;
+}
+
+void TileSet::Destroy() {
+	// Implementation goes here
+}
+
+bool TileMap::Init(const char* const filepath) {
+	// Implementation goes here
+	return true;
+}
+
+void TileMap::Destroy() {
+	// Implementation goes here
+}
